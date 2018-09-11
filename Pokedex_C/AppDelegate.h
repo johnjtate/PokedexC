68ac5c722eafd1b156b45f58326f1ca206dc96aa
John Tate <johntate@Johns-MacBook-Pro.local>
2018-09-11 10:29:52 -0600

    Initial commit

---
+++ Pokedex_C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pokedex_C
+//
+//  Created by John Tate on 9/11/18.
+//  Copyright © 2018 John Tate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
